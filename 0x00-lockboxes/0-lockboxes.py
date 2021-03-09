2d7eafeb921c5e868d5bd63888598cadb9e17868
KhaoulaDER <58639852+KhaoulaDER@users.noreply.github.com>
2021-03-09 15:52:13 +0100

    Create 0-lockboxes.py

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+  n=len(boxes)
+  L1=[i for i in range(n)]
+  for i in range(len(boxes)):
+    if (i in boxes[i]):
+      boxes[i].remove(i)
+  for box in boxes:
+    for box1 in box:
+      if ( box1 in L1):
+        L1.remove(box1)
+      else:
+        break
+  if (L1==[] or L1==[0]):
+    return True
+  else:
+    return False
