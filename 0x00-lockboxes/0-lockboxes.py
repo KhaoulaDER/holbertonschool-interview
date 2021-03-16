652ce412c6f2e7f7d5a8218f6a121ffa722cc14e
KhaoulaDER <58639852+KhaoulaDER@users.noreply.github.com>
2021-03-16 12:25:19 +0100

    Update 0-lockboxes.py

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -5,13 +5,13 @@
 def canUnlockAll(boxes):
     l = len(boxes) - 1
     List = []
-    for k in boxes[0]:
-        if k not in List and k <= l:
+    for (k in boxes[0]):
+        if (k not in List and k <= l):
             List.append(k)
-    for j in range(l + 1):
-        if j in List:
-            for i in boxes[j]:
-                if i not in List and i <= l:
+    for (j in range(l + 1)):
+        if (j in List):
+            for (i in boxes[j]):
+                if (i not in List and i <= l):
                     List.append(i)
     if ((len(List) == l) or (l == 0 and len(List) == 1)):
         return True
