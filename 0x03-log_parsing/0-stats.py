dad5c02203c3b76d73d0bf7559e8a897585ee6f5
KhaoulaDER <58639852+KhaoulaDER@users.noreply.github.com>
2022-05-20 15:02:15 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,53 +1,44 @@
 #!/usr/bin/python3
 """
-    Computes stdin input
+Log parsing: reads stdin line by line and computes metrics
 """
-
 import sys
+import signal
 
 
-def parse_data(data):
-    data = data.replace(' "GET /projects/260 HTTP/1.1"',
-                        "").replace(' -', "").replace('[', "").replace(']', "")
-    if data.count("-") == 3:
-        data = data.replace('-', " ", 1)
-    array = data.split()
-    if len(array) != 5:
-        return
-    return array
+def printstats(file_size, status_codes):
+    """print stats about file_size and status_codes"""
+    print("File size:", file_size)
+    for code in status_codes:
+        if status_dict[code] > 0:
+            print(f"{code}: {status_dict[code]}")
 
 
-i = 0
-size = 0
-status_code = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
-try:
-    for line in sys.stdin:
-        i += 1
-        array = parse_data(line)
-        if array:
-            size += int(array[4])
-            if array[3] in status_code.keys():
-                status_code[array[3]] += 1
-        if i == 10:
-            print("File size: {}".format(size))
-            for k in sorted(status_code):
-                if status_code[k] != 0:
-                    print("{}: {}".format(k, status_code[k]))
-            i = 0
-except Exception:
-    pass
-finally:
-    print("File size: {}".format(size))
-    for k in sorted(status_code):
-        if status_code[k] != 0:
-            print("{}: {}".format(k, status_code[k]))
-    i = 0
+def sigint_handler(signal, frame):
+    """Handle keyboard interrupt"""
+    printstats(file_size, status_codes)
+    sys.exit(0)
+
+
+x = 0
+file_size = 0
+status_dict = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
+
+
+for line in sys.stdin:
+    try:
+        line_split = line.split()
+        file_size += int(line_split[-1])
+        status_code = int(line_split[-2])
+        status_dict[status_code] += 1
+
+        signal.signal(signal.SIGINT, sigint_handler)
+
+        x += 1
+        if x % 10 == 0:
+            x = 0
+            printstats(file_size, status_codes)
+
+    except:
+        continue
